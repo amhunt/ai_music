1fde982a6e30967235f7796e87f99e941e0cb31d
Andrew Hunt <andrewhunt@Andrews-MacBook-Pro.local>
2016-10-29 22:51:50 -0400

    added durations to notes and added rests

--- README.md
+++ README.md
@@ -6,6 +6,6 @@ Through this project, I will be exploring various techniques for melody (and per
 ### Process
 I will be using music21 in Python to parse and generate music files. This project currently uses basic Markov chaining to produce melodies based on hundreds of input Palestrina input files.
 
-I will be saving snapshots of different generation techniques by saving different files. Hopefully this will be a good way to go back and explore which methods worked well/contributed to the quality of the generated piece. This way, as my methods of evaluation for generate scores change, I can return to precious methods and test them.
+I will be saving snapshots of different generation techniques by saving different files. The current method I'm working on will just be called *generate_music.py*. Hopefully this will be a good way to go back and explore which methods worked well/contributed to the quality of the generated piece. This way, as my methods of evaluation for generate scores change, I can return to precious methods and test them.
 
 Later in the semester I plan on exploring either counterpoint or different learning algorithms (perhaps some powerful third-party tools rather than my own Markov algorithms).
--- generate_music.py
+++ generate_music.py
@@ -2,18 +2,24 @@ from music21 import *
 import sys
 import random
 
+def get_note_or_rest( note ):
+	if note.isRest:
+		# could try duration.quarterLength as well
+		return "rest:"+note.duration.type+':'+note.duration.type
+	else:
+		return "note:"+note.nameWithOctave+':'+str(note.duration.quarterLength)
+
 def get_range_of_notes( notes, start, end ):
 	new_note_list = []
 	if end > start: 
 		for in_range_note in notes[start:end]:
-			new_note_list.append(in_range_note.midi)
+			new_note_list.append(get_note_or_rest(in_range_note))
 	else:
 		for in_range_note in notes[start:]:
-			new_note_list.append(in_range_note.midi)
+			new_note_list.append(get_note_or_rest(in_range_note))
 		for in_range_note in notes[:end]:
-			new_note_list.append(in_range_note.midi)
+			new_note_list.append(get_note_or_rest(in_range_note))
 	return new_note_list
-
 
 
 def process_score( score ):
@@ -22,23 +28,34 @@ def process_score( score ):
 		return
 	sopPart = score.parts.stream()[0]
 	notes = sopPart.flat.notesAndRests
-
 	i=0
 	for noteObj in notes:
-		# last notes
-		if (i > len(notes)-k_order):
+		if not noteObj.isRest:
+			break
+		else:
+			i+=1
+
+	notes = notes[i:]
+	return notes
+
+def process_notes( notes ):
+	i=0
+	for noteObj in notes:
+
+		if (i > len(notes)-k_order): 
+		# handle last notes (wrap-around case)
 			curr_k_notes = get_range_of_notes(notes, i, i+k_order-len(notes))
 		else:
 			curr_k_notes = get_range_of_notes(notes, i, i+k_order)
 
 		if (i >=len(notes)-k_order):
-			next_note = notes[i+k_order-len(notes)].midi
+			next_note = get_note_or_rest( notes[i+k_order-len(notes)] )
 		else:
-			next_note = notes[i+k_order].midi
+			next_note = get_note_or_rest( notes[i+k_order] )
 
-		curr_k_notes_str = str(curr_k_notes)
-		if (curr_k_notes_str in markovMap):
-			probs = markovMap[curr_k_notes_str]
+		curr_k_notes = str(curr_k_notes)
+		if (curr_k_notes in markovMap):
+			probs = markovMap[curr_k_notes]
 			if (next_note in probs):
 				probs[next_note] = probs[next_note]+1
 			else:
@@ -46,28 +63,29 @@ def process_score( score ):
 		else:
 			new_probs = {}
 			new_probs[next_note] = 1
-			markovMap[curr_k_notes_str] = new_probs
+			markovMap[curr_k_notes] = new_probs
 		i += 1
 
-def get_midis_in_range( midis, start, end ):
+def get_notes_in_range( notes, start, end ):
 	new_note_list = []
 	if end > start: 
-		for note in midis[start:end]:
+		for note in notes[start:end]:
 			new_note_list.append(note)
 	else:
-		for note in midis[start:]:
+		for note in notes[start:]:
 			new_note_list.append(note)
-		for note in midis[:end]:
+		for note in notes[:end]:
 			new_note_list.append(note)
 	return new_note_list
 
 
 def generate(first_k_notes):
-	print(":::generated music:::")
+	print("generating music")
 	i = 0
 	generated = []
-	curr_notes = get_midis_in_range(first_k_notes,0,len(first_k_notes))
+	curr_notes = get_notes_in_range(first_k_notes,0,len(first_k_notes))
 	while (i < output_length):
+		# print(i)
 		if (str(curr_notes) not in markovMap):
 			print("ERROR: " + str(curr_notes))
 			break
@@ -93,7 +111,14 @@ def generate(first_k_notes):
 	# convert to score
 	p1 = stream.Part()
 	for generatedNote in generated:
-		n = note.Note(generatedNote)
+		# print(generatedNote)
+		generatedNoteDetails = generatedNote.split(':')
+		if generatedNoteDetails[0] == "rest":
+			n = note.Rest(generatedNoteDetails[1])
+			n.duration.type = generatedNoteDetails[2]
+		else:
+			n = note.Note(generatedNoteDetails[1])
+			n.duration.quarterLength = float(generatedNoteDetails[2])
 		p1.append(n)
 	return p1
 
@@ -102,22 +127,31 @@ k_order = int(sys.argv[1])
 output_length = int(sys.argv[2])
 num_scores = int(sys.argv[3])
 
-# compile scores
+# compile scores 
 palestrina_paths = corpus.getComposer('palestrina')
 
 # add to model
 markovMap = {}
 i = 0
+halfway = False
+print("analyzing " + str(num_scores) + " scores")
 for score in palestrina_paths[:num_scores]:
-	print("analysizing score #" + str(i))
-	process_score( corpus.parse(score) )
+	score_notes = process_score( corpus.parse(score) )
+	process_notes(score_notes)
+	if (i > num_scores/2) and not halfway:
+		print("halfway done!")
+		halfway = True
+	print(i)
 	i+=1
 
 # produce new music
-first_score_notes = corpus.parse(palestrina_paths[0]).parts.stream()[0].flat.notes[0:k_order]
+first_score_notes = process_score(corpus.parse(palestrina_paths[0]))
+first_score_first_notes = first_score_notes[0:k_order]
 midi_seed = []
-for first_note in first_score_notes:
-	midi_seed.append(first_note.midi)
+for first_note in first_score_first_notes:
+	midi_seed.append(get_note_or_rest(first_note))
+
+
 generatedScore = generate(midi_seed)
 generatedScore.show()
 
